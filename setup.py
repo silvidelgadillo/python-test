1fe2fd43a8d4d354a7162f670edd24041a8a1d03
silvidelgadillo <100862618+silvidelgadillo@users.noreply.github.com>
2022-03-03 13:51:48 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,7 @@
+    name='python-test',
+    version='0.1.0',
+    url='https://github.com/silvidelgadillo/python-test',
+    author='Silvina Delgadillo',
+    author_email='sdelgadillo2000@gmail.com',
+    description='Colektia ETL process test.',
+    python_requires='3.8'
